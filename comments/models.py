aabb0a5b9514e8352729678f31286cd2c2903b3b
任绍辰 <ren06060512@gmail.com>
2023-10-02 19:18:03 -0400

    all fiil transfered

---
+++ comments/models.py
@@ -0,0 +1,42 @@
+from accounts.services import UserService
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from likes.models import Like
+from tweets.models import Tweet
+from utils.memcached_helper import MemcachedHelper
+
+
+class Comment(models.Model):
+    """
+    这个版本中，我们先实现一个比较简单的评论
+    评论只评论在某个Tweet上，不能评论别人的评论
+    """
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    tweet = models.ForeignKey(Tweet, null=True, on_delete=models.SET_NULL)
+    content = models.TextField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # 有在某个 tweet 下排序所有 comments 的需求
+        index_together = (('tweet', 'created_at'),)
+
+    def __str__(self):
+        return '{} - {} says {} at tweet {}'.format(
+            self.created_at,
+            self.user,
+            self.content,
+            self.tweet_id,
+        )
+
+    @property
+    def like_set(self):
+        return Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(Comment),
+            object_id=self.id,
+        ).order_by('-created_at')
+
+    @property
+    def cached_user(self):
+        return MemcachedHelper.get_object_through_cache(User, self.user_id)
---
+++ tweets/constants.py
@@ -0,0 +1,21 @@
+class TweetPhotoStatus:
+    PENDING = 0
+    APPROVED = 1
+    REJECTED = 2
+
+课程在  2021-9-15 号完结， 没完结的就是断更了
+网上流传20章骗人版本，就是我放出去的试看版本
+买到后第一时间确认30章后是否可以播放
+
+认准一手微信study322 其他均为翻录倒卖
+九章来offer都有  需要的+我
+课程目录
+https://www.yuque.com/study001/xk/list
+
+TWEET_PHOTO_STATUS_CHOICES = (
+    (TweetPhotoStatus.PENDING, 'Pending'),
+    (TweetPhotoStatus.APPROVED, 'Approved'),
+    (TweetPhotoStatus.REJECTED, 'Rejected'),
+)
+
+TWEET_PHOTOS_UPLOAD_LIMIT = 9
